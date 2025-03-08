9c67a25fddeb3bcd082b4adb34cfd045fdde3e14
SidM77 <siddanth.manoj@gmail.com>
2025-03-08 16:26:35 +0530

    Record to form JSON producer template

--- SidSpringBackend/src/main/java/dev/sid/sidspringbackend/Kafka/MessageRequest.java
+++ SidSpringBackend/src/main/java/dev/sid/sidspringbackend/Kafka/MessageRequest.java
@@ -1,4 +1,6 @@
 package dev.sid.sidspringbackend.Kafka;
 
-public record MessageRequest (String message) {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record MessageRequest (@JsonProperty("email") String email, @JsonProperty("id") String id) {
 }
