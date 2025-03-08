a718594a3ec0e42129e23e198194a8e2ceb59cca
SidM77 <siddanth.manoj@gmail.com>
2025-03-08 16:41:21 +0530

    Consumer config logic has been shifted outside of Listener

--- MailMicroservice/src/main/java/dev/sid/mailmicroservice/Kafka/KafkaListener.java
+++ MailMicroservice/src/main/java/dev/sid/mailmicroservice/Kafka/KafkaListener.java
@@ -1,21 +1,38 @@
 package dev.sid.mailmicroservice.Kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.sid.mailmicroservice.Service.SendEmailService;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class KafkaListener {
+
+    public SendEmailService sendEmailService;
+
+    public KafkaListener(SendEmailService sendEmailService) {
+        this.sendEmailService = sendEmailService;
+    }
+
+
     @org.springframework.kafka.annotation.KafkaListener(topics = "oralTestTopic", groupId = "groupId")
     public void listener (String message) throws JsonProcessingException {
         System.out.println("Listener received "+message);
@@ -28,29 +45,5 @@ public class KafkaListener {
         sendEmailService.sendEmail(email, "Please use this link to give Round 1 of the test http://localhost:5173/round1/"+ id , "Invitation to Interview Round 1");
     }
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
 
-    public Map<String, Object> consumerConfig () {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return props;
-    }
-
-    @Bean
-    public ConsumerFactory<String, String> consumerFactory () {
-        return new DefaultKafkaConsumerFactory<>(consumerConfig());
-    }
-
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String,String>> kafkaListenerContainerFactory (
-            ConsumerFactory<String, String> consumerFactory
-    ) {
-        ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        concurrentKafkaListenerContainerFactory.setConsumerFactory(consumerFactory);
-
-        return concurrentKafkaListenerContainerFactory;
-    }
 }
