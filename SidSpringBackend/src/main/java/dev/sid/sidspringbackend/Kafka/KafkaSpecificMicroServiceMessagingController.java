064f5ba2302901b6e22488fefa1496be89848ead
SidM77 <siddanth.manoj@gmail.com>
2025-03-08 16:25:59 +0530

    New methods added to handle sending of singleTestLink and multipleTestLinks, these requests then sent to Kafka

--- SidSpringBackend/src/main/java/dev/sid/sidspringbackend/Kafka/KafkaSpecificMicroServiceMessagingController.java
+++ SidSpringBackend/src/main/java/dev/sid/sidspringbackend/Kafka/KafkaSpecificMicroServiceMessagingController.java
@@ -16,7 +16,18 @@ public class KafkaSpecificMicroServiceMessagingController {
     private KafkaTemplate<String, String> kafkaTemplate;
     @PostMapping("api/v1/messages")
     public void publish (@RequestBody MessageRequest messageRequest) {
-        kafkaTemplate.send("oralTestTopic", messageRequest.message());
-        System.out.println("At producer currently");
+        kafkaTemplate.send("oralTestTopic", messageRequest);
+        System.out.println("At producer currently "+ messageRequest.toString()
+        );
+    }
+
+    @PostMapping("api/v1/multipleMailsTesting")
+    public void publishMailsTest (@RequestBody List<MessageRequest> messageRequest) {
+//        kafkaTemplate.send("oralTestTopic", messageRequest);
+        for (MessageRequest message : messageRequest) {
+            kafkaTemplate.send("oralTestTopic", message);
+
+            System.out.println("At producer currently "+ message.toString());
+        }
     }
 }
