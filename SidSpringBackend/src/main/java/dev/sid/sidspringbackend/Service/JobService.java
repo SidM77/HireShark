3e185b6bc69c126ed2af438b13076fa6ac70a5a4
SidM77 <siddanth.manoj@gmail.com>
2025-03-16 15:01:45 +0530

    Contains service methods along with business logic to generate the humanReadableJobIds

---
+++ SidSpringBackend/src/main/java/dev/sid/sidspringbackend/Service/JobService.java
@@ -0,0 +1,60 @@
+package dev.sid.sidspringbackend.Service;
+
+import dev.sid.sidspringbackend.Model.Job;
+import dev.sid.sidspringbackend.Repository.JobRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+public class JobService {
+
+    private final JobRepository jobRepository;
+
+    public JobService(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+
+    public Job addNewJob(Job job) {
+        String humanReadableJobId = "JX" + getBase36(5);
+        job.setHumanReadableJobId(humanReadableJobId);
+        return jobRepository.save(job);
+    }
+
+    public List<Job> findAllJobs() {
+        return jobRepository.findAll();
+    }
+
+
+    private static final char[] BASE62_CHARS =
+            "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toCharArray();
+
+    private static final Random RANDOM = new Random();
+
+    public static String getBase62(int length) {
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            sb.append(BASE62_CHARS[RANDOM.nextInt(62)]);
+        }
+
+        return sb.toString();
+    }
+
+    public static String getBase36(int length) {
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            sb.append(BASE62_CHARS[RANDOM.nextInt(36)]);
+        }
+
+        return sb.toString();
+    }
+
+    public Optional<Job> findJobByHumanReadableJobId(String humanReadableJobId) {
+        return jobRepository.findByHumanReadableJobId(humanReadableJobId);
+    }
+}
