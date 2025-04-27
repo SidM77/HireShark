ed9e3692b20c33db2976ecf931921f0bad6b5391
SidM77 <siddanth.manoj@gmail.com>
2025-04-27 18:37:33 +0530

    redundant

--- SidSpringBackend/src/main/java/dev/sid/sidspringbackend/Kafka/redundant/MessageRequest.java
+++ SidSpringBackend/src/main/java/dev/sid/sidspringbackend/Kafka/redundant/MessageRequest.java
@@ -1,6 +1,4 @@
-package dev.sid.sidspringbackend.Kafka;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
+package dev.sid.sidspringbackend.Kafka.redundant;
 
 //public record MessageRequest (@JsonProperty("email") String email, @JsonProperty("id") String id) {
 //}
