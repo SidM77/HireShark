1cbf6fbc3c4f309ed65f705b85262480076de51f
SidM77 <siddanth.manoj@gmail.com>
2025-04-27 18:38:02 +0530

    Added new topic for finalRound invite

--- SidSpringBackend/src/main/java/dev/sid/sidspringbackend/Kafka/KafkaTopicConfig.java
+++ SidSpringBackend/src/main/java/dev/sid/sidspringbackend/Kafka/KafkaTopicConfig.java
@@ -27,4 +27,8 @@ public class KafkaTopicConfig {
     public NewTopic richTechnicalTopic() {
         return TopicBuilder.name("richTechnicalTestTopic").build();
     }
+
+    public NewTopic finalRoundTopic() {
+        return TopicBuilder.name("finalRoundTopic").build();
+    }
 }
